87666f136bb464ef8aa71aa9ecf35f1dd885a821
PrachiRameshGodase <prachigodase2@gmail.com>
2023-06-21 14:09:25 +0530

    Doing Network calls and handling response the right way

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
 const showcart=useSelector(state=>state.ui.cartIsVisible)
+
+const cart =useSelector((state)=>state.cart)
+useEffect(()=>{
+  fetch("https://redux-cart-68955-default-rtdb.firebaseio.com/cart.json",{
+    method:"PUT",
+    body:JSON.stringify(cart)
+  })
+},[cart])
   return (
     <Layout>
       {showcart && <Cart />}
